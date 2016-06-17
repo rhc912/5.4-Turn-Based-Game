c500ce039354de2dd6c9cbc4323f033306581420
rhc912 <rhc912@gmail.com>
2016-06-17 14:43:46 -0400

    created meat and veggies arrays. Also made slight adjustment to html.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,3 +3,21 @@
 
 var $ = require('jquery');
 var _ = require('underscore');
+
+var models = require('./models');
+var detailTemplate = require('../templates/fighter-detail.hbs');
+var listTemplate = require('../templates/fighter-list.hbs');
+
+
+//Main Characters
+var meats =
+[new models.Hero('beef'),
+ new models.Hero('turkey'),
+ new models.Hero('chicken')
+ ];
+console.log(meats);
+ var veggies = [
+   new models.Enemy('tofu'),
+   new models.Enemy('soy'),
+   new models.Enemy('carrots')
+ ];
