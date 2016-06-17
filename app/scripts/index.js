46d90cb21a3a7f1660bde3e58b7bc77b40a4aab3
Kurt Heinritz <kmheinritz@gmail.com>
2016-06-17 14:44:41 -0400

    added models, templates, and comments

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,3 +2,29 @@
 var $ = require('jquery');
 var _ = require('underscore');
 var models = require('./models');
+
+/*
+Provide instructions for playing the game?
+
+Player chooses a warrior character, to play as, from pull down menu by clicking on warrior name.
+
+Clicking on Warrior causes that warrior to display.
+Clicking on Warrior causes warrior health to be displayed.
+Clicking on Warrior causes an enemy image to be randomly selected.
+
+clicking on Warrior causes Enemy health to be displayed.
+
+Player begins play by clicking ‘combat’ button.
+Clicking combat button causes random number generator to generate a number.
+Random number is subtracted from Enemy’s health index.
+Decremented Enemy health index is displayed(?)
+
+The random number generator is called again to generate a number.
+Random number is subtracted from Warrior health.
+Decremented Warrior health is displayed.
+
+Player starts another round of play by clicking button again.
+
+Play ends when either character’s health <= 0.
+
+*/
--- app/scripts/models.js
+++ app/scripts/models.js
@@ -1,1 +1,33 @@
 var $ = require('jquery');
+
+function Fighters(config){
+  config = config || {};
+  $.extend(this, config);
+  this.health = config.health || 60;
+  this.name = config.name;
+  //this.battleCry = config.battleCry;
+}
+
+Fighters.prototype.attack = function(victim) {
+  var damage = _.random(20);
+  victim.health = victim.health - damage;
+};
+
+
+function Hero(config){
+  Fighter.call(this, config);
+}
+
+Hero.prototype = new Fighters();
+
+function Enemy(config){
+  Fighter.call(this, config);
+}
+
+Enemy.prototype = new Fighters();
+
+
+module.exports = {
+  'Hero': Hero,
+  'Enemy': Enemy
+};
